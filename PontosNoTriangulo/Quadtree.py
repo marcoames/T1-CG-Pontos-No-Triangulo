335e894b665d932f10c8f7f7454b4f7559f3192e
Marco Ames <marco.ames@edu.pucrs.br>
2022-09-07 20:08:37 -0300

    update quadtree

--- PontosNoTriangulo/Quadtree.py
+++ PontosNoTriangulo/Quadtree.py
@@ -1,29 +1,45 @@
-class Quadtree:
+from PontosNoTriangulo.Ponto import Ponto
+from PontosNoTriangulo.Poligonos import Polygon
+from PontosNoTriangulo.PontosNoTriangulo import PontosDoCenario
 
-    class Node:
-        def __init__(self, x, y, pontos):
-            self.x = x
-            self.y = y
-            self.pontos = pontos
+class Node:
 
-        def setX(self, x):
-            self.x = x
+    # lista de filhos
+    #filhos = [null, null, null, null]
 
-        def setY(self, y):
-            self.y = y
+    def __init__(self, numPontos):
+        self.filhos = [None, None, None, None]
+        self.numPontos = numPontos
+        self.max = Ponto()
+        self.min = Ponto()
 
-        def setPontos(self, pontos):
-            self.pontos = pontos    
-             
+class Quadtree:     
     
-    def __init__(self, NodeNW, NodeNE, NodeSW, NodeSE):
-        self.NodeNW = NodeNW
-        self.NodeNE = NodeNE
-        self.NodeSW = NodeSW
-        self.NodeSE = NodeSE
+    def __init__(self, pontos, Envelope: Polygon):
+        # pontos do cenario
+        numPontos = PontosDoCenario.getNVertices()
+
+        # Nodo raiz
+        raiz = Node()
+        self.raiz = raiz
+        raiz.filhos[0] = Node(10)
+        raiz.filhos[1] = Node(10)
+        raiz.filhos[2] = Node(10)
+        raiz.filhos[3] = Node(10)
+
+        self.pontos = pontos
+        self.Envelope = Envelope
+        
+    def insereNodo():
+        pass
+    
+    def deveDividir(self):
+        if self.numPontos > 10:
+            return True
+        return False    
 
     def set():
         pass
 
-    def get():
-        pass
+    def getFilhos(node):
+        return node.filhos
